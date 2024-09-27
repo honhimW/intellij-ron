41b762a48ecf53134514f3a45c3d540f9f6cab04
honhimW <honhimw@outlook.com>
2024-09-28 01:18:34 +0800

    build: upgrade gradle 8.10.2, ide 2024.2

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        maven {
+            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+        gradlePluginPortal()
+    }
+}
+rootProject.name = "ron"
+
