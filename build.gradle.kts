4dd6287f1edf7721b9497e3b0b31cf597a5cbef9
honhimW <honhimw@outlook.com>
2024-09-28 01:34:30 +0800

    build: upgrade gradle 8.10.2, ide 2024.2

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.tasks.PatchPluginXmlTask
+
+plugins {
+    id("java")
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+}
+
+group = "org.jonahhenriksson"
+version = "1.5"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("src/main/gen")
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2")
+
+        bundledPlugin("com.intellij.java")
+
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+
+        testFramework(TestFrameworkType.Platform)
+    }
+
+    testImplementation("junit:junit:4.13.2")
+}
+
+
+intellijPlatform {
+
+    pluginConfiguration {
+        changeNotes.set("""Updated to support 2024.2 edition of Intellij IDEs""")
+    }
+
+    publishing {
+        token.set(System.getenv("INTELLIJ_MARKETPLACE_TOKEN"))
+    }
+}
